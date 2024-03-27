1c46ca3fc89280eaacf167f44ac48e257c702a5a
ericsson chan <ericssonchann@gmail.com>
2024-03-27 21:00:40 +0800

    added files for follower/following feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users, only: [:show]
+  get 'dashboard', to: 'dashboard#index'
+
 
 end
