df817e5dd9793079cc218709978645b28fac3ecf
ericsson chan <ericssonchann@gmail.com>
2024-03-27 20:31:47 +0800

    Added files for follow/followers feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :subscriptions, as: :subscribable
+
+  has_many :followers_subscriptions, foreign_key: :subscribable_id, class_name: "Subscription"
+  has_many :followers, through: :followers_subscriptions, source: :user
+
+  has_many :following_subscriptions, foreign_key: :user_id, class_name: "Subscription"
+  has_many :following, through: :following_subscriptions, source: :subscribable, source_type: 'User'
 end
