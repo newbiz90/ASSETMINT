0515254158d47bf65d749d052f79059f26e15e0d
Matthew Oh <newbiz90@hotmail.com>
2024-03-30 13:36:12 +0800

    for_chloe_3

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 class TransactionsController < ApplicationController
 
   def index
-    @alltxn = Transaction.all
+    @mytxns = Transaction.joins(:user_ticker).where(user_ticker:{user: current_user})
     @ticker = Ticker.all
     # raise
   end
