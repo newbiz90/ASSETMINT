c22fb56798f63f8fee386bc0068ea46146b63d50
Chloe Lee <leejiayun.chloe@gmail.com>
2024-04-03 22:09:39 +0800

    Added script to visited user page to try and create subscription. Not yet complete

--- app/javascript/watchlist.js
+++ app/javascript/watchlist.js
@@ -1,6 +1,8 @@
 const allHearts = document.querySelectorAll(".fa-regular");
 console.log(allHearts);
 
+// console.log(otherUser);
+
 allHearts.forEach( heart => {
   heart.addEventListener("click", follow => {
     // console.log(follow.target.classList);
