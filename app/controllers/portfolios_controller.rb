fa5202f16f3712b83cb8d57cb7be012ab51b20ae
Zong Han Goh <zonghangoh@gmail.com>
2024-04-13 16:25:47 +0800

    fix

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,11 +4,17 @@ class PortfoliosController < ApplicationController
 
     @followers_count = @user.followers.count
     @following_count = @user.following.count
-    @usertxns = Transaction.where(user_ticker: @user.user_tickers)
-    @transactions = @user.transactions
     @alltickers = Ticker.all
     @txn_count = 0
     @usertickers = UserTicker.all
+
+    if params.dig(:search, :ticker_name).present?
+      ticker = Ticker.find_by(name: params[:search][:ticker_name])
+      filtered_user_ticker = UserTicker.where(ticker:, user: @user)
+      @usertxns = Transaction.where(user_ticker: filtered_user_ticker)
+    else
+      @usertxns = @user.transactions
+    end
   end
 
   def me
