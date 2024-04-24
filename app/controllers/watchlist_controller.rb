6504bdb49e6af14e0518e92dd87428efdc37fad3
Zong Han Goh <zonghangoh@gmail.com>
2024-04-24 22:00:51 +0800

    watchlist fixes

--- app/controllers/watchlist_controller.rb
+++ app/controllers/watchlist_controller.rb
@@ -1,6 +1,8 @@
 class WatchlistController < ApplicationController
   def show
     @tickerSubs = current_user.subscriptions.where(subscribable_type: "UserTicker")
+
+    @transactions = Transaction.where(user_ticker_id: @tickerSubs.map(&:subscribable_id))
   end
 
   def followtickerform
